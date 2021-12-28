0c81a8cae44ae2f69f399dcbd3dcc912d018eb17
hakangulcu <hakan.gulcu@ug.bilkent.edu.tr>
2021-12-28 22:52:05 +0300

    table fix

--- src/main/front/ClubManagerMainPage.js
+++ src/main/front/ClubManagerMainPage.js
@@ -1,6 +1,8 @@
 const tbody = document.getElementById("tbody");
 const tobbybest1 = document.getElementById("tobbybest");
 const clubID = localStorage.getItem("clubId");
+const memberTable = document.getElementById("memberTable");
+const activityTable = document.getElementById("activityTable");
 console.log("club id is " + clubID);
 
 function getData() {
@@ -14,6 +16,14 @@ function getData() {
                 let surname = response.data[i].surname;
                 let id1 = response.data[i].id;
 
+                var row = memberTable.insertRow(i+1);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                cell1.innerHTML = name;
+                cell2.innerHTML = surname;
+                cell3.innerHTML = id;
+                /**
                 // TODO: name, surname ve id'yi ayırın
                 const line31 = document.createElement('line31');
                 line31.innerHTML = '<tr  style="color: red"><td class="ms-4" style="color: red">' + name + '</td><td>' +
@@ -21,6 +31,7 @@ function getData() {
                     '<tr  style="color: red"><td class="ms-4" style="color: red">' + id1 + '</td><td>';
                     // surname + '</td><td>' + id1 + '</td> </tr>';
                 tbody.appendChild(line31);
+                 */
             }
         }
     )
@@ -49,17 +60,26 @@ function getData() {
                 }
                 organizators += list[list.length - 1].clubName;
 
+                var row = activityTable.insertRow(i+1);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                cell1.innerHTML = date;
+                cell2.innerHTML = activityName;
+                cell3.innerHTML = organizators;
+
                 /*for(let j = 0; j < activity.organizerClubList.length - 1; j++) {
                     organizators += activity.organizerClubList[j].clubName + " | ";
                 }
                 organizators += activity.organizerClubList[organizerClubList.length - 1].clubName;
 
 
-                 */
+                
 
                 const line32 = document.createElement('line32');
                 line32.innerHTML = '<tr><td>' + date + '</td><td>' + desc + '</td><td>' + organizators + '</td> </tr>';
                 tobbybest1.appendChild(line32);
+                 */
             }
         }
     )
--- src/main/front/ClubMembersPage.js
+++ src/main/front/ClubMembersPage.js
@@ -1,5 +1,6 @@
 const tbody = document.getElementById("tbody");
 const id = sessionStorage.getItem("clubId");
+const clubMemberTable = document.getElementById("clubMembersTable");
 console.log(id);
 
 
@@ -17,10 +18,28 @@ const getData = () => {
                 var sid = response.data[i].userId;
                 var email = response.data[i].email;
                 var dep = response.data[i].department;
-                const line31 = document.createElement('line31');
+
+
+                var row = clubMemberTable.insertRow(i+1);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
+                var cell4 = row.insertCell(4);
+
+                cell1.innerHTML = name;
+                cell2.innerHTML = surname;
+                cell3.innerHTML = userId;
+                cell4.innerHTML = email;
+                cell5.innerHTML = department;
+
+                /**
+                  const line31 = document.createElement('line31');
                 line31.innerHTML = '<tr><th scope="col">' + name + '</th><th scope="col"> ' + surname + '</th><th scope="col">' + sid + '</th><th scope="col">' + email + '</th><th scope="col">' + dep + '</th><td style="text-align: center;"><button class="remove">Remove from Club</button></td></tr>';
                 tbody.appendChild(line31);
-            }
+                }
+                 */
+               
             ;
         }
     )
