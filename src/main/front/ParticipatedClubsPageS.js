f9b3379cbc539295f8fcf3dddf11c8f203073ece
Berk Baltaci <berk.baltaci@ug.bilkent.edu.tr>
2021-12-29 01:20:10 +0300

    Update ParticipatedClubsPageS.js

--- src/main/front/ParticipatedClubsPageS.js
+++ src/main/front/ParticipatedClubsPageS.js
@@ -11,11 +11,22 @@ const getData = () => {
         for (let i = 0; i < response.data.length; i++) {
 
             let clubname = response.data[i].clubName;
+            let clubID = response.data[i].id;
 
-            document.body.innerHTML += '<ul class="list-group list-group-horizontal mt-3 justify-content-evenly" style="background-color: white"  >                <li class="list-group-item col-lg-4 col-sm-4 col-md-4 text-center ms-2" style="border: none;background-color:white">'+ clubname +'</li> <li class="list-group-item  col-lg-2 ms-2 " style="border: none;background-color:white"><button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Show</button> </li><li class="list-group-item col-lg-2 ms-2" style="border: none;background-color:white"> <button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Join</button> </li> <li class="list-group-item col-lg-2 ms-2" style="border: none;background-color:white"> <button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Add to Favorite</button> </li> </ul>';
+            document.body.innerHTML += '<ul class="list-group list-group-horizontal mt-3 justify-content-evenly" style="background-color: white"  >                <li class="list-group-item col-lg-4 col-sm-4 col-md-4 text-center ms-2" style="border: none;background-color:white">'+ clubname +'</li> <li class="list-group-item  col-lg-2 ms-2 " style="border: none;background-color:white"><button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Show</button> </li><li class="list-group-item col-lg-2 ms-2" style="border: none;background-color:white"> <button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Leave</button> </li> <li class="list-group-item col-lg-2 ms-2" style="border: none;background-color:white"> <button onclick = "addFavorite('+studentID+ ','+clubID+')" "type="button" class="btn btn-danger btn-outline-dark " style="color: white">Add to Favorite</button> </li> </ul>';
 
         }
     })
 };
 
+
+function addFavorite(studentID,clubID) {
+   axios.get("https://projectdeneme.herokuapp.com/changeFavoriteStatus/"+ studentID + "/" + clubID )
+       .then(function (response){
+           console.log(response)
+       })
+       .catch(function (error) {
+           console.log(error)
+       })
+}
 
